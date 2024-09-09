85e33daf37476d04f38338ec6a17f78e6c5990e9
alex.luelmo <alex.luelmo@students.salle.url.edu>
2024-09-09 17:09:57 +0200

    style: update home section padding top

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <section id="home" className="py-20">
+    <section id="home" className="pb-20 py-32">
       <h1 className="text-4xl font-black mb-4">Welcome to My Portfolio</h1>
       <p className="text-xl">
         I'm a passionate developer creating amazing web experiences
