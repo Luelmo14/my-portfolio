23c834108cbf7e12087d4904f8907cdc88fe2602
Alex Luelmo <alexitoluelmito@gmail.com>
2024-09-19 17:58:32 +0200

    refactor: extract certification data to constants file

---
+++ src/constants/certificationData.ts
@@ -0,0 +1,56 @@
+const certificationData = [
+  {
+    id: "dataAnalysis",
+    issuer: "freeCodeCamp",
+    credentialId: "fcc987a7545-306c-4828-875c-6b603a809ddb-dawp",
+    credentialLink:
+      "https://www.freecodecamp.org/certification/fcc987a7545-306c-4828-875c-6b603a809ddb/data-analysis-with-python-v7",
+    logo: "src/assets/images/freeCodeCamp.webp",
+    skillLogo: "src/assets/images/analysis.webp",
+    translate: "translate-x-1.5 translate-y-1.5 -rotate-17",
+    size: "w-32 h-32",
+    padding: "py-[2px]",
+  },
+  {
+    id: "google1",
+    issuer: "Google Actívate",
+    credentialId: "306312174",
+    credentialLink:
+      "https://skillshop.exceedlms.com/student/award/1bwb6pNByceD1ZBCxoqFWXmA",
+    logo: "src/assets/images/google.webp",
+    skillLogo: "src/assets/images/css.webp",
+    translate: "translate-x-0 translate-y-3.5 -rotate-12",
+    size: "w-[120px]",
+  },
+  {
+    id: "google2",
+    issuer: "Google Actívate",
+    credentialId: "306303461",
+    credentialLink:
+      "https://skillshop.exceedlms.com/student/award/jiVh7R2mi5kuMarVmWCquauW",
+    logo: "src/assets/images/google.webp",
+    skillLogo: "src/assets/images/html.webp",
+    translate: "translate-x-0 translate-y-3.5 -rotate-12",
+    size: "w-[120px]",
+  },
+  {
+    id: "pcep",
+    issuer: "OpenEDG Python Institute",
+    credentialId: "k5M3.bwpf.J8Ys",
+    credentialLink: "https://verify.openedg.org/?id=k5M3.bwpf.J8Ys",
+    logo: "src/assets/images/openEDG.webp",
+    skillLogo: "src/assets/images/python.webp",
+    translate: "translate-x-7 translate-y-[2.1rem] -rotate-17",
+    size: "w-40 h-40",
+  },
+  {
+    id: "b2",
+    issuer: "Escola d'Idiomes de Montgat",
+    logo: "src/assets/images/first.webp",
+    skillLogo: "src/assets/images/cambridge.webp",
+    translate: "-translate-x-1 translate-y-6 -rotate-17",
+    size: "w-[100px]",
+  },
+];
+
+export default certificationData;
