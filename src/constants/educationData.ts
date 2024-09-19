2f80d1757d8bc8fe71758bb0f95287665f517bbd
Alex Luelmo <alexitoluelmito@gmail.com>
2024-09-19 17:56:14 +0200

    refactor: extract education data to constants file

---
+++ src/constants/educationData.ts
@@ -0,0 +1,25 @@
+const educationData = [
+  {
+    id: "lasalle",
+    dateRange: "2020 — 2023",
+    logo: "src/assets/images/laSalle.webp",
+    padding: "p-1",
+    website: "https://www.salleurl.edu/",
+  },
+  {
+    id: "stucom",
+    dateRange: "2018 — 2020",
+    logo: "src/assets/images/stucom.webp",
+    padding: "p-3",
+    website: "https://www.stucom.com/",
+  },
+  {
+    id: "thalassa",
+    dateRange: "2016 — 2018",
+    logo: "src/assets/images/thalassa.webp",
+    padding: "p-0.5",
+    website: "https://agora.xtec.cat/iesthalassa/",
+  },
+];
+
+export default educationData;
