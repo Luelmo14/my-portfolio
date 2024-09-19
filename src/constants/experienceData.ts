804116c63c1a198e3e28336a23bcfb5e4a852f78
Alex Luelmo <alexitoluelmito@gmail.com>
2024-09-19 17:57:24 +0200

    refactor: extract experience data to constants file

---
+++ src/constants/experienceData.ts
@@ -0,0 +1,20 @@
+const experienceData = [
+  {
+    id: "nexiona",
+    company: "Nexiona Connectocrats, S.L.",
+    dateRange: "2023 — 2024",
+    logo: "src/assets/images/nexiona.webp",
+    padding: "p-2",
+    website: "https://nexiona.com/es/",
+  },
+  {
+    id: "fujitsu",
+    company: "Fujitsu Technology Solutions, SA",
+    dateRange: "2019 — 2020",
+    logo: "src/assets/images/fujitsu.webp",
+    padding: "p-1",
+    website: "https://global.fujitsu/es-es",
+  },
+];
+
+export default experienceData;
