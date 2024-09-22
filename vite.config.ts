e68ff4e88c88b0c19b3a66abb218994dbdef55e7
alex.luelmo <alex.luelmo@students.salle.url.edu>
2024-09-22 13:24:47 +0200

    feat: add Geist font to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import Unfonts from "unplugin-fonts/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Unfonts({
-      custom: {
-        families: [
-          {
-            name: "Geist",
-            src: "./src/assets/fonts/*.woff2",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
